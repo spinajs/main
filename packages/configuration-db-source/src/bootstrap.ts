f450eb535aee3953ecbeba0f4779d5d6bf6017cb
Grzegorz Beń <grzegorz.ben@coderush.pl>
2022-02-13 20:49:41 +0100

    invalid import

--- packages/configuration-db-source/src/bootstrap.ts
+++ packages/configuration-db-source/src/bootstrap.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/require-await */
 import { Injectable, Bootstrapper, DI } from '@spinajs/di';
-import { ConfiguratioDbSourceInitialMigration } from './migrations/configuration_db_source_2022_02_08_01_13_00';
+import { configuration_db_source_2022_02_08_01_13_00 } from './migrations/configuration_db_source_2022_02_08_01_13_00';
 import { DbConfigurationModel } from './models/DbConfigurationModel';
 import CONFIGURATION_SCHEMA from './schemas/configuration.db.source.schema';
 
@@ -9,7 +9,7 @@ export class DbConfigSourceBotstrapper extends Bootstrapper {
   public async bootstrap(): Promise<void> {
     DI.register(CONFIGURATION_SCHEMA).asValue('__configurationSchema__');
     DI.register(DbConfigurationModel).asValue('__model__');
-    DI.register(ConfiguratioDbSourceInitialMigration).asValue('__migration__');
+    DI.register(configuration_db_source_2022_02_08_01_13_00).asValue('__migration__');
     return;
   }
 }
