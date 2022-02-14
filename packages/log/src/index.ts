24075c9c393ae539850165e7f7aaff68b41b3c78
Grzegorz Beń <grzegorz.ben@coderush.pl>
2022-02-14 15:03:24 +0100

    removed unnessesery exports

--- packages/log/src/index.ts
+++ packages/log/src/index.ts
@@ -1,18 +1,3 @@
-/**
- * We export default configuration for webpack modules
- * Normally we load configuration from disk via filesystem
- * But webpack is bundlig all files into one.
- *
- * When we export, we can see configuration variable
- * in webpack module cache and webpack config loader can see it
- */
-export * as config from "./config/log";
-
-/**
- * Same issue with schemas
- */
-export * as CONFIGURATION_SCHEMA from "./schemas/log.configuration";
-
 export * from "@spinajs/log-common";
 export * from "./targets";
 export * from "./variables";
