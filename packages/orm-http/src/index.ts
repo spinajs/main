225a0a7f4fedd0ffc0b6a8114d3380ab4d59ab3a
Grzegorz Beń <grzegorz.ben@coderush.pl>
2022-06-29 15:45:05 +0200

    orm http extension - fill arg as db model

---
+++ packages/orm-http/src/index.ts
@@ -0,0 +1,47 @@
+import { Orm } from '@spinajs/orm';
+import { IRouteArgs, IRouteParameter, IRouteCall, Parameter, Route, ParameterType } from '@spinajs/http';
+import { AsyncModule, IContainer, Injectable, Container, Autoinject } from '@spinajs/di';
+import * as express from 'express';
+
+@Injectable()
+export class FromDbModel extends AsyncModule implements IRouteArgs {
+  @Autoinject(Container)
+  protected Container: IContainer;
+
+  @Autoinject(Orm)
+  protected Orm: Orm;
+
+  async resolveAsync(): Promise<void> {}
+
+  public get SupportedType(): string {
+    return 'FromDB';
+  }
+
+  public async extract(callData: IRouteCall, param: IRouteParameter, req: express.Request) {
+    const model = this.Orm.Models.find((m) => m.name === param.RuntimeType);
+    let p: any = null;
+
+    switch (param.Options.type) {
+      case ParameterType.FromQuery:
+        p = req.query[param.Options.field ?? param.Name];
+        break;
+      case ParameterType.FromBody:
+        p = req.body[param.Options.field ?? param.Name];
+        break;
+      case ParameterType.FromHeader:
+        p = req.headers[param.Options.field ?? param.Name.toLowerCase()];
+        break;
+      case ParameterType.FromParams:
+      default:
+        p = req.params[param.Options.field ?? param.Name];
+        break;
+    }
+
+    const result = await model.type['findOrFail'](p);
+    return { CallData: callData, Args: result };
+  }
+}
+
+export function FromModel(field?: string, type?: ParameterType) {
+  return Route(Parameter('FromDbModel', null, { field, type }));
+}
