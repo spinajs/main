0a53241b409fea0e575f8c7f65f47dad1f08fb2e
bemon <grzegorz.ben@coderush.pl>
2022-07-07 14:02:34 +0200

    v1.2.212

--- packages/configuration-db-source/src/models/DbConfigurationModel.ts
+++ packages/configuration-db-source/src/models/DbConfigurationModel.ts
@@ -9,7 +9,6 @@ import { DateTime } from 'luxon';
 @Connection('default')
 @Model('configuration')
 export class DbConfigurationModel extends ModelBase {
-
   @Primary()
   public Id: number;
 
@@ -21,35 +20,34 @@ export class DbConfigurationModel extends ModelBase {
 
   public Type: 'int' | 'float' | 'string' | 'json' | 'date' | 'datetime' | 'time' | 'boolean';
 
-  public hydrate(data: Partial<this>) { 
-    Object.assign(this, {...data, Value: this.parse(data.Value as string) })
+  public hydrate(data: Partial<this>) {
+    Object.assign(this, { ...data, Value: this.parse(data.Value as string) });
   }
 
   public dehydrate(_includeRelations?: boolean, _omit?: string[]) {
-      return {
-           Id: this.Id,
-         Slug: this.Slug,
-        Group: this.Group,
-         Type: this.Type,
-        Value: this.stringify(this.Value)
-      } as any;
+    return {
+      Id: this.Id,
+      Slug: this.Slug,
+      Group: this.Group,
+      Type: this.Type,
+      Value: this.stringify(this.Value),
+    } as any;
   }
 
-  private parse(input : string){
-    switch(this.Type){
-        case 'int':
-        case 'float':
-          return  Number(input);
-        case 'datetime':
-          return DateTime.fromISO(input);
-        case "time":
-          return DateTime.fromFormat(input, "HH:mm:ss");
-        case "date": 
-          return DateTime.fromFormat(input, "dd-MM-YYYY");
-        case "json":
-          return JSON.parse(input) as unknown;
+  private parse(input: string) {
+    switch (this.Type) {
+      case 'int':
+      case 'float':
+        return Number(input);
+      case 'datetime':
+        return DateTime.fromISO(input);
+      case 'time':
+        return DateTime.fromFormat(input, 'HH:mm:ss');
+      case 'date':
+        return DateTime.fromFormat(input, 'dd-MM-YYYY');
+      case 'json':
+        return JSON.parse(input) as unknown;
         break;
-         
     }
   }
 
@@ -57,19 +55,18 @@ export class DbConfigurationModel extends ModelBase {
     switch (this.Type) {
       case 'json':
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        return this.Value = JSON.stringify(val);
+        return (this.Value = JSON.stringify(val));
       case 'date':
-        return this.Value = (val as DateTime).toFormat('dd-MM-YYYY');
+        return (this.Value = (val as DateTime).toFormat('dd-MM-YYYY'));
       case 'time':
-        return this.Value = (val as DateTime).toFormat('HH:mm:ss');
+        return (this.Value = (val as DateTime).toFormat('HH:mm:ss'));
       case 'datetime':
-        return this.Value = (val as DateTime).toISO();
+        return (this.Value = (val as DateTime).toISO());
       case 'string':
       case 'int':
       case 'float':
       default:
         return `${val}`;
-    
     }
   }
 }
