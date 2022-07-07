d32d4559f3115c83a78579ff3ad7992b91495fff
bemon <grzegorz.ben@coderush.pl>
2022-07-07 13:46:18 +0200

    db config model proper fill

--- packages/configuration-db-source/src/models/DbConfigurationModel.ts
+++ packages/configuration-db-source/src/models/DbConfigurationModel.ts
@@ -6,7 +6,6 @@ import { DateTime } from 'luxon';
 @Connection('default')
 @Model('configuration')
 export class DbConfigurationModel extends ModelBase {
-  private _realValue: number | string | DateTime | boolean | unknown;
 
   @Primary()
   public Id: number;
@@ -19,32 +18,55 @@ export class DbConfigurationModel extends ModelBase {
 
   public Type: 'int' | 'float' | 'string' | 'json' | 'date' | 'datetime' | 'time' | 'boolean';
 
-  public get RealValue(): number | string | DateTime | boolean | unknown {
-    return this._realValue;
+  public hydrate(data: Partial<this>) { 
+    Object.assign(this, {...data, Value: this.parse(data.Value as string) })
   }
 
-  public set RealValue(val: number | string | DateTime | boolean | unknown) {
-    this._realValue = val;
+  public dehydrate(_includeRelations?: boolean, _omit?: string[]) {
+      return {
+           Id: this.Id,
+         Slug: this.Slug,
+        Group: this.Group,
+         Type: this.Type,
+        Value: this.stringify(this.Value)
+      } as any;
+  }
 
+  private parse(input : string){
+    switch(this.Type){
+        case 'int':
+        case 'float':
+          return  Number(input);
+        case 'datetime':
+          return DateTime.fromISO(input);
+        case "time":
+          return DateTime.fromFormat(input, "HH:mm:ss");
+        case "date": 
+          return DateTime.fromFormat(input, "dd-MM-YYYY");
+        case "json":
+          return JSON.parse(input);
+        break;
+         
+    }
+  }
+
+  private stringify(val: number | string | DateTime | boolean | unknown) {
     switch (this.Type) {
+      case 'json':
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        return this.Value = JSON.stringify(val);
+      case 'date':
+        return this.Value = (val as DateTime).toFormat('dd-MM-YYYY');
+      case 'time':
+        return this.Value = (val as DateTime).toFormat('HH:mm:ss');
+      case 'datetime':
+        return this.Value = (val as DateTime).toISO();
       case 'string':
       case 'int':
       case 'float':
-        this.Value = `${val}`;
-        break;
-      case 'json':
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        this.Value = JSON.stringify(val);
-        break;
-      case 'date':
-        this.Value = (val as DateTime).toFormat('dd-MM-YYYY');
-        break;
-      case 'time':
-        this.Value = (val as DateTime).toFormat('HH:mm:ss');
-        break;
-      case 'datetime':
-        this.Value = (val as DateTime).toISO();
-        break;
+      default:
+        return `${val}`;
+    
     }
   }
 }
