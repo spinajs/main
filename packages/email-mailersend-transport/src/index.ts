c8bfeeb6e4caf1184d1fdbcdaaa6b4271a23a8fb
bemon <grzegorz.ben@coderush.pl>
2022-09-13 21:02:56 +0200

    vebump

--- packages/email-mailersend-transport/src/index.ts
+++ packages/email-mailersend-transport/src/index.ts
@@ -1,27 +1,27 @@
-import { Autoinject, Injectable, NewInstance } from '@spinajs/di';
-import { Log, Logger } from '@spinajs/log';
-import { Email, EmailSender, EmailConnectionOptions } from '@spinajs/email';
-import { Templates } from '@spinajs/templates';
-import { fs } from '@spinajs/fs';
-import _ from 'lodash';
+// import { Autoinject, Injectable, NewInstance } from '@spinajs/di';
+// import { Log, Logger } from '@spinajs/log';
+// import { Email, EmailSender, EmailConnectionOptions } from '@spinajs/email';
+// import { Templates } from '@spinajs/templates';
+// import { fs } from '@spinajs/fs';
+// import _ from 'lodash';
 
-@Injectable()
-@NewInstance()
-export class MailersendTransport extends EmailSender {
-  @Logger('email')
-  protected Log: Log;
+// @Injectable()
+// @NewInstance()
+// export class MailersendTransport extends EmailSender {
+//   @Logger('email')
+//   protected Log: Log;
 
-  @Autoinject(Templates)
-  protected Tempates: Templates;
+//   @Autoinject(Templates)
+//   protected Tempates: Templates;
 
-  @Autoinject(fs, (x) => x.Provider)
-  protected FileSystems: Map<string, fs>;
+//   @Autoinject(fs, (x) => x.Provider)
+//   protected FileSystems: Map<string, fs>;
 
-  constructor(public Options: EmailConnectionOptions) {
-    super();
-  }
+//   constructor(public Options: EmailConnectionOptions) {
+//     super();
+//   }
 
-  public async resolveAsync(): Promise<void> {}
+//   public async resolveAsync(): Promise<void> {}
 
-  public async send(email: Email): Promise<void> {}
-}
+//   public async send(email: Email): Promise<void> {}
+// }
