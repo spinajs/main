13af6f96841be8ac76fb80eec63b4b80e82f341f
bemon <grzegorz.ben@coderush.pl>
2022-10-16 15:56:36 +0200

    changes

--- packages/rbac-http/src/middlewares/AttributeFilter.ts
+++ packages/rbac-http/src/middlewares/AttributeFilter.ts
@@ -1,17 +1,23 @@
-import { BaseMiddleware, IController, IRoute } from '@spinajs/http';
+import { RouteMiddleware, IController, IRoute, Response } from '@spinajs/http';
 import { Request } from 'express';
 
 /**
  * Filters attributes of db models
  */
-export class FilterAttribute extends BaseMiddleware {
+export class FilterAttribute extends RouteMiddleware {
+  public onResponse(response: Response, route: IRoute): Promise<void> {
+
+     
+
+  }
+
   public isEnabled(_action: IRoute, _instance: IController): boolean {
     return true;
   }
 
   // tslint:disable-next-line: no-empty
-  public async onBeforeAction(_req: Request): Promise<void> {}
+  public async onBefore(): Promise<void> {}
 
   // tslint:disable-next-line: no-empty
-  public async onAfterAction(_req: Request): Promise<void> {}
+  public async onAfter(): Promise<void> {}
 }
