3e63b2ef6665f6bdca7861611dfa51ae08698bd7
bemon <grzegorz.ben@coderush.pl>
2022-11-11 15:07:25 +0100

    empty queue as default in config

--- packages/queue/src/config/queue.ts
+++ packages/queue/src/config/queue.ts
@@ -12,4 +12,16 @@ module.exports = {
     //   models: [dir('./../models')],
     // },
   },
+  queue: {
+    // by default, all messages are sent to black hole
+    // becouse some modules use queues to pass events eg. user login
+    // but not always in project we want to use queue or have queue server
+    default: 'black-hole',
+    connections: [
+      {
+        name: 'black-hole',
+        transport: 'BlackHoleQueueClient',
+      },
+    ],
+  },
 };
