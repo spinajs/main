0d7a1d78c893d52a4c2100893def2bff4e2c56c2
bemon <grzegorz.ben@coderush.pl>
2023-02-08 14:06:12 +0100

    ver bump & builder return type fix

--- packages/orm-api/src/config/orm-http.ts
+++ packages/orm-api/src/config/orm-http.ts
@@ -1,4 +1,7 @@
+import { DI } from '@spinajs/di';
 import { join, normalize, resolve } from 'path';
+
+const isESMMode = DI.get<boolean>('__esmMode__');
 
 function dir(path: string) {
   return resolve(normalize(join(process.cwd(), path)));
@@ -7,8 +10,8 @@ function dir(path: string) {
 const ormHttp = {
   system: {
     dirs: {
-      controllers: [dir('./../controllers')],
-      schemas: [dir('./../schemas')],
+      controllers: [dir(`node_modules/@spinajs/orm-http/lib/${isESMMode ? 'mjs/controllers' : 'cjs/controllers'}`)],
+      schemas: [dir(`node_modules/@spinajs/orm-http/lib/${isESMMode ? 'mjs/schemas' : 'cjs/schemas'}`)],
     },
   },
 
@@ -16,9 +19,9 @@ const ormHttp = {
     endpoint: {
       transformer: {
         service: 'JsonApiCollectionTransofrmer',
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
 export default ormHttp;
