efeef2c7e703ddd8c53ef93058783c247a978c96
bemon <grzegorz.ben@coderush.pl>
2023-05-10 14:07:06 +0200

    tests for crud api

--- packages/orm-api/test/api.get.test.ts
+++ packages/orm-api/test/api.get.test.ts
@@ -69,61 +69,100 @@ describe('crud get tests', function () {
     sinon.restore();
   });
 
-  it('GET /:model', async () => {
-    const result = await req().get('collection/test').set('Accept', 'application/json').send();
-    expect(result).to.have.status(200);
+  describe('GET methods as guest', function () {
+    it('GET /:model should return forbidden', async () => {});
+    it('GET /:model/:id should return forbidden', async () => {});
+    it('GET /:model/:id/:relation should return forbidden', async () => {});
+    it('GET /:model/:id/:relation/:id should return forbidden', async () => {});
 
-    const data = JSON.parse(result.text);
-    expect(data.Data).to.be.an('array');
-    expect(data.Data).to.have.length(3);
-    expect(data.Data[0].Id).to.eq(1);
-    expect(data.Data[1].Id).to.eq(2);
-    expect(data.Data[2].Id).to.eq(3);
+    it('GET /:model', async () => {
+      const result = await req().get('collection/test').set('Accept', 'application/json').send();
+      expect(result).to.have.status(200);
+
+      const data = JSON.parse(result.text);
+      expect(data.Data).to.be.an('array');
+      expect(data.Data).to.have.length(3);
+      expect(data.Data[0].Id).to.eq(1);
+      expect(data.Data[1].Id).to.eq(2);
+      expect(data.Data[2].Id).to.eq(3);
+    });
+
+    it('GET /:model/:id', async () => {
+      const result = await req().get('collection/test/1').set('Accept', 'application/json').send();
+      expect(result).to.have.status(200);
+
+      const data = JSON.parse(result.text);
+      expect(data.Id).to.eq(1);
+    });
+
+    it('GET /:model/:id/:relation', async () => {
+      const result = await req().get('collection/test/1/teststwos').set('Accept', 'application/json').send();
+      expect(result).to.have.status(200);
+
+      const data = JSON.parse(result.text);
+      expect(data.Data).to.be.an('array');
+      expect(data.Data).to.have.length(2);
+      expect(data.Data[0].Id).to.eq(1);
+      expect(data.Data[1].Id).to.eq(2);
+      expect(data.Data[0].Text).to.eq('Test2-1');
+      expect(data.Data[1].Text).to.eq('Test2-2');
+    });
+    it('GET /:model/:id/:relation/:id', async () => {
+      const result = await req().get('collection/test/1/teststwos/1').set('Accept', 'application/json').send();
+      expect(result).to.have.status(200);
+
+      const data = JSON.parse(result.text);
+      expect(data.Id).to.eq(1);
+      expect(data.Text).to.eq('Test2-1');
+    });
+
+    it('GET /:model/:id with & include', async () => {
+      const result = await req().get('collection/test/1?includes=TestsTwos,Belongs').set('Accept', 'application/json').send();
+      expect(result).to.have.status(200);
+
+      const data = JSON.parse(result.text);
+      expect(data.Id).to.eq(1);
+      expect(data.Text).to.eq('Test-1');
+
+      expect(data.Belongs).to.be.not.undefined;
+      expect(data.Belongs.Text).to.eq('Belongs-1');
+      expect(data.TestsTwos).to.be.not.undefined;
+      expect(data.TestsTwos).to.be.an('array');
+      expect(data.TestsTwos).to.have.length(2);
+      expect(data.TestsTwos[0].Text).to.eq('Test2-1');
+      expect(data.TestsTwos[1].Text).to.eq('Test2-2');
+    });
   });
 
-  it('GET /:model/:id', async () => {
-    const result = await req().get('collection/test/1').set('Accept', 'application/json').send();
-    expect(result).to.have.status(200);
+  describe('GET methods as logged user', function () {
+    describe('Get any', function () {
+      it('GET /:model should return forbidden', async () => {});
+      it('GET /:model/:id should return forbidden', async () => {});
+      it('GET /:model/:id/:relation should return forbidden', async () => {});
+      it('GET /:model/:id/:relation/:id should return forbidden', async () => {});
+      it('GET /:model/:id with & include should return forbidden ', async () => {});
 
-    const data = JSON.parse(result.text);
-    expect(data.Id).to.eq(1);
-  });
+      it('GET /:model', async () => {});
+      it('GET /:model/:id', async () => {});
+      it('GET /:model/:id/:relation', async () => {});
+      it('GET /:model/:id/:relation/:id', async () => {});
+    });
 
-  it('GET /:model/:id/:relation', async () => {
-    const result = await req().get('collection/test/1/teststwos').set('Accept', 'application/json').send();
-    expect(result).to.have.status(200);
+    describe('Get own', function () {
+      it('GET /:model should return forbidden', async () => {});
+      it('GET /:model/:id should return forbidden', async () => {});
+      it('GET /:model/:id/:relation should return forbidden', async () => {});
+      it('GET /:model/:id/:relation/:id should return forbidden', async () => {});
+      it('GET /:model/:id with & include should return forbidden ', async () => {});
 
-    const data = JSON.parse(result.text);
-    expect(data.Data).to.be.an('array');
-    expect(data.Data).to.have.length(2);
-    expect(data.Data[0].Id).to.eq(1);
-    expect(data.Data[1].Id).to.eq(2);
-    expect(data.Data[0].Text).to.eq('Test2-1');
-    expect(data.Data[1].Text).to.eq('Test2-2');
-  });
-  it('GET /:model/:id/:relation/:id', async () => {
-    const result = await req().get('collection/test/1/teststwos/1').set('Accept', 'application/json').send();
-    expect(result).to.have.status(200);
+      it('GET /:model', async () => {});
+      it('GET /:model/:id', async () => {});
+      it('GET /:model/:id/:relation', async () => {});
+      it('GET /:model/:id/:relation/:id', async () => {});
+      it('GET /:model/:id with & include', async () => {});
 
-    const data = JSON.parse(result.text);
-    expect(data.Id).to.eq(1);
-    expect(data.Text).to.eq('Test2-1');
-  });
-
-  it('GET /:model/:id with & include', async () => {
-    const result = await req().get('collection/test/1?includes=TestsTwos,Belongs').set('Accept', 'application/json').send();
-    expect(result).to.have.status(200);
-
-    const data = JSON.parse(result.text);
-    expect(data.Id).to.eq(1);
-    expect(data.Text).to.eq('Test-1');
-
-    expect(data.Belongs).to.be.not.undefined;
-    expect(data.Belongs.Text).to.eq('Belongs-1');
-    expect(data.TestsTwos).to.be.not.undefined;
-    expect(data.TestsTwos).to.be.an('array');
-    expect(data.TestsTwos).to.have.length(2);
-    expect(data.TestsTwos[0].Text).to.eq('Test2-1');
-    expect(data.TestsTwos[1].Text).to.eq('Test2-2');
+      it('GET /:model should be filtered', async () => {});
+      it('GET /:model/:id/:relation should be filtered', async () => {});
+    });
   });
 });
