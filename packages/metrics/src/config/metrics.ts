7a134d30b5aa8d10e60d58b8c5dc74becc7ebfd8
bemon <grzegorz.ben@coderush.pl>
2023-06-15 11:19:01 +0200

    added controllers in metrics config

--- packages/metrics/src/config/metrics.ts
+++ packages/metrics/src/config/metrics.ts
@@ -1,35 +1,43 @@
+import { join, normalize, resolve } from 'path';
+
+function dir(path: string) {
+  return resolve(normalize(join(process.cwd(), path)));
+}
 
 /**
- * 
+ *
  * Configuraiton for prom metrics bundle for express
  * https://github.com/jochen-schweizer/express-prom-bundle
- *  
+ *
  */
 const config = {
-    metrics: {
-        auth: { 
-
-            // set this in your project
-            token: "",
-            policy: "DefaultMetricsPolicy"
-        },
-
-        /**
-         *  * Configuraiton for prom metrics bundle for express
-         * https://github.com/jochen-schweizer/express-prom-bundle
-         */
-        http: { 
-            includeStatusCode: true,
-            includeMethod: true,
-            includePath: true,
-           // customLabels: "spinajs",
-           // includeUp:  true,
-           // metricsPath: "/metrics",
-           // metricType: "histogram",
-           // httpDurationMetricName: 60
-        }
+  system: {
+    dirs: {
+      controllers: [dir('./../controllers')],
     },
-  };
-  
-  export default config;
-  
+  },
+  metrics: {
+    auth: {
+      // set this in your project
+      token: '',
+      policy: 'DefaultMetricsPolicy',
+    },
+
+    /**
+     *  * Configuraiton for prom metrics bundle for express
+     * https://github.com/jochen-schweizer/express-prom-bundle
+     */
+    http: {
+      includeStatusCode: true,
+      includeMethod: true,
+      includePath: true,
+      // customLabels: "spinajs",
+      // includeUp:  true,
+      // metricsPath: "/metrics",
+      // metricType: "histogram",
+      // httpDurationMetricName: 60
+    },
+  },
+};
+
+export default config;
