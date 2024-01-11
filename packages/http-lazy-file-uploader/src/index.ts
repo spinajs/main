eb67f694915d18aeff8e656367cda6fa43563c27
bemon <grzegorz.ben@coderush.pl>
2024-01-11 16:15:49 +0100

    lazy file upload fixing

--- packages/http-lazy-file-uploader/src/index.ts
+++ packages/http-lazy-file-uploader/src/index.ts
@@ -1,33 +1,27 @@
 import { Injectable } from '@spinajs/di';
-import { FormFileUploader, IUploadedFile } from "@spinajs/http";
+import { FormFileUploader, IUploadedFile } from '@spinajs/http';
 import { Log, Logger } from '@spinajs/log-common';
-import { QueueClient } from '@spinajs/queue';
 import { LazyUploadJob } from './job.js';
 
-
-@Injectable(FormFileUploader)
+@Injectable()
 export class HttpLazyFileUploader extends FormFileUploader {
+  @Logger('http')
+  protected Log: Log;
 
-    @Logger('http')
-    protected Log: Log;
+  constructor(public Options: { fs: string }) {
+    super();
+  }
 
+  public async upload(file: IUploadedFile) : Promise<IUploadedFile<any>> {
+    await LazyUploadJob.emit({
+      ToFilesystem: this.Options.fs,
+      Path: file.BaseName,
+      SourceFilesystem: file.Provider.Name,
+    });
 
-    constructor(public Filesystem: string) {
-        super();
-
-    }
-
-    public async upload(file: IUploadedFile) {
-
-        await LazyUploadJob.emit({
-            ToFilesystem: this.Filesystem,
-            Path: file.BaseName,
-            SourceFilesystem: file.Provider.Name
-        });
-
-        return {
-            ...file,
-            Provider: null
-        };
-    }
+    return {
+      ...file,
+      Provider: null,
+    };
+  }
 }
--- packages/http-lazy-file-uploader/src/job.ts
+++ packages/http-lazy-file-uploader/src/job.ts
@@ -1,47 +1,54 @@
-import { DI } from "@spinajs/di";
-import { IOFail } from "@spinajs/exceptions";
-import { fs } from "@spinajs/fs";
-import { Log, Logger } from "@spinajs/log-common";
-import { QueueJob, Job } from "@spinajs/queue";
+import { DI } from '@spinajs/di';
+import { IOFail } from '@spinajs/exceptions';
+import { fs } from '@spinajs/fs';
+import { Log, Logger } from '@spinajs/log-common';
+import { QueueJob, Job } from '@spinajs/queue';
 
 @Job()
 export class LazyUploadJob extends QueueJob {
+  @Logger('LazyFileUploader')
+  protected Log: Log;
 
-    @Logger('LazyFileUploader')
-    protected Log: Log;
+  // Filesystem to upload to
+  public ToFilesystem: string;
 
-    // Filesystem to upload to
-    public ToFilesystem: string;
+  public Path: string;
 
-    public Path: string;
+  public SourceFilesystem: string;
 
-    public SourceFilesystem: string;
+  /**
+   * Should source file be deleted after upload
+   */
+  public DeleteAfterUpload: boolean;
 
-    public async execute(progress: (p: number) => Promise<void>) {
+  public async execute(progress: (p: number) => Promise<void>) {
+    const sFs = DI.resolve<fs>('__file_provider__', [this.SourceFilesystem]);
+    const tFs = DI.resolve<fs>('__file_provider__', [this.ToFilesystem]);
 
-        const sFs = DI.resolve<fs>("__file_provider__", [this.SourceFilesystem]);
-        const tFs = DI.resolve<fs>("__file_provider__", [this.ToFilesystem]);
-
-        if (!sFs) {
-            throw new IOFail(`Filesystem ${this.SourceFilesystem} not exists, pleas check your configuration`);
-        }
-
-        if (!tFs) {
-            throw new IOFail(`Filesystem ${this.ToFilesystem} not exists, pleas check your configuration`);
-        }
-
-        this.Log.trace(`Copying ${this.Path} from ${this.SourceFilesystem} to ${this.ToFilesystem}`);
-        this.Log.timeStart(`[COPY] ${this.Path}`);
-
-        await progress(0);
-
-        await sFs.copy(this.Path, this.Path, tFs);
-
-        await progress(100);
-
-        const d = this.Log.timeEnd(`[COPY] ${this.Path}`);
-        this.Log.trace(`Copied ${this.Path} from ${this.SourceFilesystem} to ${this.ToFilesystem} in ${d}ms`);
-
-        return 'finished';
+    if (!sFs) {
+      throw new IOFail(`Filesystem ${this.SourceFilesystem} not exists, pleas check your configuration`);
     }
+
+    if (!tFs) {
+      throw new IOFail(`Filesystem ${this.ToFilesystem} not exists, pleas check your configuration`);
+    }
+
+    this.Log.trace(`Copying ${this.Path} from ${this.SourceFilesystem} to ${this.ToFilesystem}`);
+    this.Log.timeStart(`[COPY] ${this.Path}`);
+
+    await progress(0);
+
+    await sFs.copy(this.Path, this.Path, tFs);
+
+    if (this.DeleteAfterUpload) {
+      await sFs.rm(this.Path);
+    }
+
+    await progress(100);
+
+    const d = this.Log.timeEnd(`[COPY] ${this.Path}`);
+    this.Log.trace(`Copied ${this.Path} from ${this.SourceFilesystem} to ${this.ToFilesystem} in ${d}ms`);
+
+    return 'finished';
+  }
 }
