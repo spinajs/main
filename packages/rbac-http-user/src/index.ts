e6749fd79d9602227ca51bb5adcbd4425ae9413d
bemon <grzegorz.ben@coderush.pl>
2024-06-06 13:37:54 +0200

    export fix

--- packages/rbac-http-user/src/index.ts
+++ packages/rbac-http-user/src/index.ts
@@ -1,4 +1,4 @@
 export * from './controllers/LoginController.js';
-export * from './controllers/UserController.js';
-export * from './2fa/SpeakEasy2FaToken.js';
-export * from "./fingerprint/FingerprintJs.js";
+// export * from './controllers/UserController.js';
+// export * from './2fa/SpeakEasy2FaToken.js';
+// export * from "./fingerprint/FingerprintJs.js";
