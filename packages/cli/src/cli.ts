63f0c2dcf3e3d25879186b28d70b7696dcb5209a
bemon <grzegorz.ben@coderush.pl>
2024-09-20 02:29:04 +0200

    cli now run bootstrapers

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
-import { DI } from '@spinajs/di';
+import { Bootstrapper, DI } from '@spinajs/di';
 import { Log } from '@spinajs/log';
 import { Cli } from './index.js';
 import { Configuration } from '@spinajs/configuration-common';
-import "./args.js"
-
+import './args.js';
 
 async function cli() {
-
   DI.setESMModuleSupport();
+  const bootstrappers = DI.resolve(Array.ofType(Bootstrapper));
+
+  for (const b of bootstrappers) {
+    await b.bootstrap();
+  }
+
   await DI.resolve(Configuration);
 
   const log = DI.resolve(Log, ['CLI']);
