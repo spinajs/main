5696327e3bc850db5982cc933842d2b4f0d5f1db
bemon <grzegorz.ben@coderush.pl>
2024-10-15 12:04:41 +0200

    adde fs file info to fp  functions

--- packages/fs/src/fp.ts
+++ packages/fs/src/fp.ts
@@ -1,11 +1,26 @@
 import { _resolve } from '@spinajs/di';
+import { FileInfoService } from './interfaces.js';
+import { _chain, _use } from '@spinajs/util';
 
 /**
  * Gets filesystem by its name
- * 
+ *
  * @param name filesystem name ( must be defined in config file)
- * @returns 
+ * @returns
  */
 export function _fs(name: string) {
   return _resolve('__file_provider__', [name]);
+}
+
+/**
+ * 
+ * Gets file information ( extended file information eg. movie fps, coded etc.)
+ * 
+ * @param path abs path to file
+ * @returns 
+ */
+export function _fileInfo(path: string) {
+  return _chain(_use(_resolve(FileInfoService), 'fileInfo'), ({ fileInfo }: { fileInfo: FileInfoService }) => {
+    return fileInfo.getInfo(path);
+  });
 }
