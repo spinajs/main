a0170a5b612eaf612f716703b5795dd2e856f8f3
bemon <grzegorz.ben@coderush.pl>
2025-03-28 01:34:26 +0100

    fix cache cmd

--- packages/http/src/cli/GenerateControllerCache.ts
+++ packages/http/src/cli/GenerateControllerCache.ts
@@ -1,26 +1,22 @@
 import { CliCommand, Command } from '@spinajs/cli';
 import { Logger, Log } from '@spinajs/log-common';
 import { DefaultControllerCache } from '../cache.js';
-import { Autoinject } from '@spinajs/di';
-import { FileSystem, fs as fFs } from '@spinajs/fs';
+import { DI } from '@spinajs/di';
+import { fsService, fs } from '@spinajs/fs';
 
 @Command('http:controllers:cache', 'generate controllers cache')
 export class ControllersCacheCommand extends CliCommand {
   @Logger('http')
   protected Log: Log;
 
-  /**
-   * File system for temporary files for storing controllers cache
-   */
-  @FileSystem('__fs_controller_cache__')
-  protected CacheFS: fFs;
-
-  @Autoinject()
-  protected Cache: DefaultControllerCache;
-
   public async execute(): Promise<void> {
+    await DI.resolve(fsService);
+
     this.Log.info('Generating controllers cache ...');
-    await this.Cache.generate();
-    this.Log.info('Controllers cache generated at path: ', this.CacheFS.resolvePath(''));
+
+    const fs = DI.resolve<fs>('__file_provider__', ['__fs_controller_cache__']);
+    const cache = DI.resolve(DefaultControllerCache);
+    await cache.generate();
+    this.Log.info('Controllers cache generated at path: ', fs.resolvePath(''));
   }
 }
