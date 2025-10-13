a8f215527840665a50355e5df4849617f75f7225
bemon <grzegorz.ben@coderush.pl>
2025-10-13 09:24:33 +0200

    fix SqlLiteJoinStatement after join statements refactor

--- packages/orm-sqlite/src/statements.ts
+++ packages/orm-sqlite/src/statements.ts
@@ -1,16 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { Constructor, NewInstance } from '@spinajs/di';
-import { RawQuery, JoinMethod, ModelBase, SelectQueryBuilder } from '@spinajs/orm';
+import { NewInstance } from '@spinajs/di';
+import { JoinMethod, IJoinStatementOptions } from '@spinajs/orm';
 import { SqlJoinStatement } from '@spinajs/orm-sql';
 import { NotSupported } from '@spinajs/exceptions';
 
 @NewInstance()
 export class SqlLiteJoinStatement extends SqlJoinStatement {
-  constructor(builder: SelectQueryBuilder<any>, sourceModel: Constructor<ModelBase>, table: string | RawQuery, method: JoinMethod, foreignKey: string, primaryKey: string, alias: string, tableAlias: string) {
-    super(builder, sourceModel, table, method, foreignKey, primaryKey, alias, tableAlias);
+  constructor(protected _options: IJoinStatementOptions) {
+    super(_options);
 
-    if (method === JoinMethod.RIGHT || method === JoinMethod.RIGHT_OUTER) {
-      throw new NotSupported(`join method ${method} is not supported by sqlite driver`);
+    if (_options.method === JoinMethod.RIGHT || _options.method === JoinMethod.RIGHT_OUTER) {
+      throw new NotSupported(`join method ${_options.method} is not supported by sqlite driver`);
     }
   }
 }
